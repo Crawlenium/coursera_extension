259e7336448c9722061e2cca74d178795222bae6
ndhieunguyen <hieunguyen.kientuong@gmail.com>
2023-07-30 09:54:36 +0700

    fix: sraping only the first element

--- main.js
+++ main.js
@@ -5,22 +5,27 @@ document.getElementById('start').addEventListener('click', () => {
     function mainFunc(){
         function getInfomaionInEachCertificate(element){
             element.getElementsByClassName('add-to-linkedin-section')[0].children[0].click()
-            const details = document.getElementsByClassName("rc-AccomplishmentCard card-rich-interaction")[0].children[1].children[2]
+            const details = element.children[1].children[2]
+            
+            try {
+                let name = details.getElementsByClassName('linkedin-detail-value')[0].children[0].textContent
+            } catch {
+                let name = details.getElementsByClassName('linkedin-detail-value')[0].textContent
+            }
 
-            const name = details.getElementsByClassName('linkedin-detail-value')[0].children[0].textContent
-            const issuingOrganization = details.getElementsByClassName('linkedin-detail-value')[1].textContent
-            const issueDate = details.getElementsByClassName('linkedin-detail-value')[2].textContent
-            const expirationDate = details.getElementsByClassName('linkedin-detail-value')[3].textContent
-            const credentialID = details.getElementsByClassName('linkedin-detail-value')[4].textContent
-            const credentialURL = details.getElementsByClassName('linkedin-detail-value')[5].textContent
+            let issuingOrganization = details.getElementsByClassName('linkedin-detail-value')[1].textContent
+            let issueDate = details.getElementsByClassName('linkedin-detail-value')[2].textContent
+            let expirationDate = details.getElementsByClassName('linkedin-detail-value')[3].textContent
+            let credentialID = details.getElementsByClassName('linkedin-detail-value')[4].textContent
+            let credentialURL = details.getElementsByClassName('linkedin-detail-value')[5].textContent
 
             return [name, issuingOrganization, issueDate, expirationDate, credentialID, credentialURL].join(";")
         }
 
-        const all_certificate_section = document.getElementsByClassName("rc-AccomplishmentCard card-rich-interaction");
-        const info = [];
-        for (element of all_certificate_section){
-            info.push(getInfomaionInEachCertificate(element));
+        let allCertificateSection = document.getElementsByClassName("rc-AccomplishmentCard card-rich-interaction");
+        let info = [];
+        for (ele of allCertificateSection){
+            info.push(getInfomaionInEachCertificate(ele));
         }
 
         const link = document.createElement("a");
